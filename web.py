ea265593c4f70a261dc3007b1b3d60751393c4d4
Tom Gustafsson <tom.gustafsson@iki.fi>
2026-01-04 22:50:10 +0200

    update

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -7,28 +7,15 @@ app = Flask(__name__)
 @app.route("/give", methods=["GET"])
 def run_command():
     player = request.args.get("player")
+    os.system(
+        'docker exec -it `docker container ls | grep papermc | awk "{print $1}"` ./mcrcon-0.7.2-linux-x86-64-static/mcrcon -H localhost -p minecraft "mvinv give ' + player + ' PvP minecraft:wooden_axe"',
+    )
 
-    try:
-        result = subprocess.run(
-            'docker exec -it `docker container ls | grep papermc | awk "{print $1}"` ./mcrcon-0.7.2-linux-x86-64-static/mcrcon -H localhost -p minecraft "mvinv give ' + player + ' PvP minecraft:wooden_axe"',
-            capture_output=True,
-            text=True,
-            check=True
-        )
-
-        return jsonify({
-            "stdout": result.stdout,
-            "stderr": result.stderr
-        })
-
-    except subprocess.CalledProcessError as e:
-        return jsonify({
-            "error": "Command failed",
-            "stdout": e.stdout,
-            "stderr": e.stderr
-        }), 500
+    return jsonify({
+        "stdout": 'done'
+    })
 
 
 if __name__ == "__main__":
     # Run only on localhost
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="0.0.0.0", port=8000)
